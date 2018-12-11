a7289a7cf6abf3afd9836428837264f522923bdd
Joe Wait <joe.wait@gilmond.com>
2018-12-11 12:43:31 +0000

    Add project files.

---
+++ Smets1SDKTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smets1SDKTest
+{
+    public class Program
+    {
+        public HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            var terminalhandler = new Terminal();
+            var init = new APIInit();
+            var client = init.Clientbuilder("test", "test1");
+
+            terminalhandler.InitializeTerminal();
+        }
+    }
+}
