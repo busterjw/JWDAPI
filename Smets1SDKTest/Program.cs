b6d4407c4fae5c10d9b096053765b06459ea83da
Joe Wait <joe.wait@gilmond.com>
2018-12-11 13:30:08 +0000

    Removed extra classes, added api key

--- Smets1SDKTest/Program.cs
+++ Smets1SDKTest/Program.cs
@@ -13,10 +13,15 @@ namespace Smets1SDKTest
 
         static void Main(string[] args)
         {
-            var terminalhandler = new Terminal();
-            var init = new APIInit();
-            var client = init.Clientbuilder("test", "test1");
-            terminalhandler.InitializeTerminal();
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("X-API-Key", "d2dd86f8c7224a458c4f4c47e677aaaf");
+                var response = client.GetAsync("https://www.bungie.net/Platform/Destiny2/4/Profile/4611686018485516375/").Result;
+                var content = response.Content.ReadAsStringAsync().Result;
+                dynamic item = Newtonsoft.Json.JsonConvert.DeserializeObject(content);
+
+                Console.WriteLine(item.Response.data);
+            }
         }
     }
 }
